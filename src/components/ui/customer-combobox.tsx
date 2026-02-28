416d5ea4335faa2d8d5e4a29c1675b10f3d0d0d5
TSX JSC <giangnguyenpr96@gmail.com>
2026-02-28 13:27:28 +0700

    Update customer-combobox.tsx

--- src/components/ui/customer-combobox.tsx
+++ src/components/ui/customer-combobox.tsx
@@ -75,7 +75,7 @@ export function CustomerCombobox({
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-full p-0" align="start">
-        <Command>
+        <Command shouldFilter={false}>
           <CommandInput
             placeholder="Tìm theo tên hoặc ID khách hàng..."
             value={search}
